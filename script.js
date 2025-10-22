051d1560854ad2dfb50c2b95f7281799b84cd56f
Uana <237180106+xeztonic@users.noreply.github.com>
2025-10-22 11:31:51 +0300

    learned vars

---
+++ script.js
@@ -0,0 +1,22 @@
+// add 2 numbers 
+console.log(13+15)
+
+// add 6 numbers
+console.log(10+90+10+2+3+5)
+
+// log expression
+console.log((4+6+9)/77)
+
+// variables
+let a=10
+console.log(a)
+a=5
+console.log(a)
+let b=7*a
+console.log(b)
+
+// sequence
+const max = 57
+const actual = max - 13
+const percentage = actual/max
+console.log(percentage)
