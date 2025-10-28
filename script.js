1f28e26dfd7956315e981715f59ea77bddac52df
Uana <237180106+xeztonic@users.noreply.github.com>
2025-10-28 13:57:04 +0200

    last modification

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-// add 2 numbers 
-console.log(13+15)
+// // add 2 numbers 
+// console.log(13+15)
 
-// add 6 numbers
-console.log(10+90+10+2+3+5)
+// // add 6 numbers
+// console.log(10+90+10+2+3+5)
 
-// log expression
-console.log((4+6+9)/77)
+// // log expression
+// console.log((4+6+9)/77)
 
-// variables
-let a=10
-console.log(a)
-a=5
-console.log(a)
-let b=7*a
-console.log(b)
+// // variables
+// let a=10
+// console.log(a)
+// a=5
+// console.log(a)
+// let b=7*a
+// console.log(b)
 
-// sequence
-const max = 57
-const actual = max - 13
-const percentage = actual/max
-console.log(percentage)
+// // sequence
+// const max = 57
+// const actual = max - 13
+// const percentage = actual/max
+// console.log(percentage)
+
+
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(4 + 5 + "px");
+console.log("$" + 4 + 5);
+console.log("4" - 2);
+console.log("4px" - 2);
+console.log("  -9  " + 5);
+console.log("  -9  " - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(" \t \n" - 2);
