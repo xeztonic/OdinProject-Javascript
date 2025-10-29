3129e0f3489052ca4d919972506dcddbcb1e462b
Uana <237180106+xeztonic@users.noreply.github.com>
2025-10-29 11:57:56 +0200

    solved 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let result = "";
+    for (let i = 0; i < times; i++) {
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
