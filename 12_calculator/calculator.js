55c30f079891a6eb70f7014c087d058632600091
Uana <237180106+xeztonic@users.noreply.github.com>
2025-10-29 14:08:02 +0200

    solved 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let suma = 0;
+  arr.forEach(element => {
+    suma += element;
+  });
+
+  return suma;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  let prod = 1;
+  arr.forEach(element => {
+    prod *= element;
+  });
 
+  return prod;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a == 0) return 1;
+
+  let prod2 = 1;
+  for (let i = 1; i <= a; i++)
+    prod2 *= i;
+
+  return prod2;
 };
 
 // Do not edit below this line
