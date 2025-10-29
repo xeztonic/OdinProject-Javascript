85e9496ce2758c0f44fc3834ff0c920b938bbc09
Uana <237180106+xeztonic@users.noreply.github.com>
2025-10-29 13:13:01 +0200

    solved 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+    if (int1 < 0 || int2 < 0) return 'ERROR';
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) return 'ERROR';
 
+    const start = Math.min(int1, int2);
+    const end = Math.max(int1, int2);
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
