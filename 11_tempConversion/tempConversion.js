742d485d807e77e4bc277268fe2fb136e54cfb73
Uana <237180106+xeztonic@users.noreply.github.com>
2025-10-29 13:49:31 +0200

    solved 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahGrades) {
+  const result = (fahGrades - 32) * (5 / 9);
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celGrades) {
+  const result = celGrades * (9 / 5) + 32;
+  return Math.round(result * 10) / 10
 };
 
 // Do not edit below this line
